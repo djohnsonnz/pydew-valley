8447495fa31d188a950e1f85f34261fb0827ffa5
Daniel Johnson <9582890+djohnsonnz@users.noreply.github.com>
2023-11-15 13:50:06 +1300

    feat: Add overlay for selected tool and seed

--- code/settings.py
+++ code/settings.py
@@ -4,3 +4,9 @@ SCREEN_WIDTH = 1270
 SCREEN_HEIGHT = 720
 TITLE_SIZE = 64
 
+# overlay positions
+OVERLAY_POSITIONS = {
+    'tool' : (40, SCREEN_HEIGHT - 15),
+    'seed' : (70, SCREEN_HEIGHT - 5)
+}
+
