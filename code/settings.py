d29047869fcce542c41625ba8c6245f914892f35
Daniel Johnson <9582890+djohnsonnz@users.noreply.github.com>
2023-11-16 13:23:22 +1300

    feat: Configure the camera and add the map

--- code/settings.py
+++ code/settings.py
@@ -10,3 +10,19 @@ OVERLAY_POSITIONS = {
     'seed' : (70, SCREEN_HEIGHT - 5)
 }
 
+LAYERS = {
+    'water' : 0,
+    'ground' : 1,
+    'soil' : 2,
+    'soil water' : 3,
+    'rain floor' : 4,
+    'house bottom' : 5,
+    'ground plant' : 6,
+    'main' : 7,
+    'house top' : 8,
+    'fruit' : 9,
+    'rain drops' : 10,
+}
+
+
+
