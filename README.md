bb5a93d3b28665937928e92d91797bed668b7245
Daniel Johnson <9582890+djohnsonnz@users.noreply.github.com>
2023-11-18 17:20:45 +1300

    feat: Add ability to create soil tile on farmable tiles

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-Learning Python and PyGame <br>
-Testing Conventional Commits
+# Learning Python and PyGame and Conventional Commits
+
+## Features to add once done
+- Pick apples instead of use axe
+- Add date or number of days
--- code/level.py
+++ code/level.py
@@ -6,6 +6,7 @@ from sprites import Generic, Water, WildFlower, Tree, Interaction
 from pytmx.util_pygame import load_pygame
 from support import *
 from transition import Transition
+from soil import SoilLayer
 
 class Level:
     def __init__(self):
@@ -19,9 +20,12 @@ class Level:
         self.tree_sprites = pygame.sprite.Group()
         self.interaction_sprites = pygame.sprite.Group()
 
+        self.soil_layer = SoilLayer(self.all_sprites)
+
         self.setup()
         self.overlay = Overlay(self.player)
         self.transition = Transition(self.reset, self.player)
+
 
     def setup(self):
         tmx_data = load_pygame('../data/map.tmx')
@@ -64,7 +68,9 @@ class Level:
                     group = self.all_sprites,
                     collision_sprites = self.collision_sprites,
                     tree_sprites = self.tree_sprites,
-                    interaction = self.interaction_sprites)
+                    interaction = self.interaction_sprites,
+                    soil_layer = self.soil_layer
+                )
 
             if obj.name == 'Bed':
                 Interaction((obj.x, obj.y), (obj.width, obj.height), self.interaction_sprites, obj.name)
